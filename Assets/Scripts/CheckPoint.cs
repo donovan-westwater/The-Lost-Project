c630e5c22067f96d5de8938ebcef05c9726a44cc
anthonygrooms <anthonygroomswork@gmail.com>
2020-01-31 23:38:02 -0500

    Added checkpoint system'

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public bool Activated { get; set; }
+    public int number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Activated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CheckPointSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointSystem : MonoBehaviour
+{
+    public CheckPoint[] checkPoints; // Array of checkpoints
+
+    // Get the transform of the most recent checkpoint
+    public Vector3 GetRecetCheckPointTransform()
+    {
+        for (int i = checkPoints.Length - 1; i >= 1; i--)
+            if (checkPoints[i].Activated)
+                return checkPoints[i].gameObject.transform.position;
+        return checkPoints[0].gameObject.transform.position;
+    }
+}
