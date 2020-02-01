d5e97a9ce3c7e3dd99d869131c3bd6652ecefd28
brunno <brunnofirme@gmail.com>
2020-01-31 23:04:12 -0500

    Revert

---
+++ Assets/Glitch_Box.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glitch_Box : GlitchObject
+{
+    bool isCollidable;
+
+    public override void RefreshData(string data)
+    {
+        Debug.Log("help");
+    }
+}
