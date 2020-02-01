33c4db2e8f5934a7cb6be09a816bf1a4482bfcd7
brunno <brunnofirme@gmail.com>
2020-01-31 21:44:41 -0500

    Slightly less Bad file reading system

--- Assets/TestingPlayerInteraction.cs
+++ Assets/TestingPlayerInteraction.cs
@@ -6,6 +6,9 @@ using System.Security.AccessControl;
 
 public class TestingPlayerInteraction : MonoBehaviour
 {
+    /*
+     * The plan is to have the player type in their directory in the beginning of the game and then reference that directory for the rest of the game this current test CustomFilePath is just for us to test before we implement the player text input field
+     * */
     string m_Path;
     string CustomFilePath = "D:/Profiles/student/LostGame/Yes";
     string customFileName = "/Level1Test";
@@ -16,7 +19,6 @@ public class TestingPlayerInteraction : MonoBehaviour
 
         //Get the path of the Game data folder
         m_Path = Application.dataPath;
-        m_Path = m_Path.Replace("/Assets", "");
 
         string m_Path_With_String = CustomFilePath + customFileName;
 
