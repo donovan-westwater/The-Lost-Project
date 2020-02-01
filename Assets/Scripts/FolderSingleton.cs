69faa24e3a5daefdec90f5d95edaf7640acb43ed
Brunno Firme <brunnofirme@gmail.com>
2020-02-01 12:27:03 -0500

    a

---
+++ Assets/Scripts/FolderSingleton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolderSingleton : MonoBehaviour
+{
+    public static FolderSingleton instance;
+    
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
