fb1c987e81c080d10e34074948f14d424cb5ebe0
Donovan Westwater <donovan.westwater@gmail.com>
2020-02-01 14:00:09 -0500

    Have a basic version of the tutorial document object. Might start working on designing the actual challange for tutorial and level 1

---
+++ Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTrigger : MonoBehaviour
+{
+    GameObject Player;
+    public Image img;
+    public Text txt;
+    bool popupmode = false;
+    public float triggerRadius = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        //this.GetComponent<Image>().enabled = false;
+        img.enabled = false;
+        txt.enabled = false;
+       // this.GetComponent<Text>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.eulerAngles += new Vector3(0, Time.deltaTime * 5f, 0);
+        if(Vector3.Distance(Player.transform.position,this.transform.position) < triggerRadius && !popupmode)
+        {
+            popupmode = true;
+            PopUpToggle(true);
+        }
+        else if (popupmode && Input.GetKeyDown(KeyCode.E))
+        {
+            PopUpToggle(false);
+            
+        }
+        else if(Vector3.Distance(Player.transform.position, this.transform.position) > triggerRadius)
+        {
+            popupmode = false;
+            PopUpToggle(false);
+        }
+        
+    }
+    void PopUpToggle(bool b)
+    {
+        //popupmode = b;
+        img.enabled = b;
+        txt.enabled = b;
+    }
+
+}
