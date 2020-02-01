6be1c5d5aa22689239d10e01f8572be0e0f85259
brunno <brunnofirme@gmail.com>
2020-01-31 20:01:02 -0500

    The start

---
+++ Assets/TestingPlayerInteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingPlayerInteraction : MonoBehaviour
+{
+    string m_Path;
+
+    void Start()
+    {
+        //Get the path of the Game data folder
+        m_Path = Application.dataPath;
+
+        //Output the Game data path to the console
+        Debug.Log("dataPath : " + m_Path);
+    }
+}
