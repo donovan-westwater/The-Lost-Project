f0567c4aa53c4dfd7e91feceee6bb73d32251463
brunno <brunnofirme@gmail.com>
2020-01-31 21:38:59 -0500

    Bad file reading system

--- Assets/TestingPlayerInteraction.cs
+++ Assets/TestingPlayerInteraction.cs
@@ -1,17 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Security.AccessControl;
 
 public class TestingPlayerInteraction : MonoBehaviour
 {
     string m_Path;
+    string CustomFilePath = "D:/Profiles/student/LostGame/Yes";
+    string customFileName = "/Level1Test";
 
     void Start()
     {
+        customFileName += ".txt";
+
         //Get the path of the Game data folder
         m_Path = Application.dataPath;
+        m_Path = m_Path.Replace("/Assets", "");
+
+        string m_Path_With_String = CustomFilePath + customFileName;
 
         //Output the Game data path to the console
         Debug.Log("dataPath : " + m_Path);
+
+        //File.Copy(m_Path2 + "/Pog.txt", CustomFilePath, false);
+        File.WriteAllText(m_Path_With_String, "I have created txt");
+
+
+        /*
+        File.Create(CustomFilePath + "/SubSubFolder/folder.txt");
+        File.Move(m_Path_With_String, "");
+        */
+        
+        /*
+        if (!File.Exists(m_Path_With_String))
+        {
+            File.WriteAllText(m_Path, "I have created txt");
+        }
+        */
+        
+        
     }
 }
