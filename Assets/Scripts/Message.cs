5e347edf2c7b40583d2d8668fa67afc5eeb391c7
anthonygrooms <anthonygroomswork@gmail.com>
2020-02-01 19:26:06 -0500

    Tutorial Edits

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour
+{
+    private Text text;
+    public int messageNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        if (messageNumber == 0)
+        {
+            text.text = "";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
