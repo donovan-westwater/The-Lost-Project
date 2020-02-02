63c77b5e5559f7838c0b54ee9c3c2c54ca741f37
Brunno Firme <brunnofirme@gmail.com>
2020-02-02 14:43:37 -0500

    FolderSing

--- Assets/Scripts/FolderSingleton.cs
+++ Assets/Scripts/FolderSingleton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System.IO;
 using System.Security.AccessControl;
 using System;
-
+using static System.Environment;
 public class FolderSingleton : MonoBehaviour
 {
     public static FolderSingleton instance;
@@ -23,9 +23,9 @@ public class FolderSingleton : MonoBehaviour
             Destroy(this);
         }
 
-        if (sourceFilePath == "")
+        if (playerSelectedFilePath == "")
         {
-            sourceFilePath = "f";
+            playerSelectedFilePath = "f";
         }
 
         try
@@ -36,18 +36,37 @@ public class FolderSingleton : MonoBehaviour
         catch (Exception)
         {
             playerSelectedFilePath = Application.dataPath + "/Result";
+
+
         }
 
+
         sourceFilePath = Application.dataPath + "/Source";
+
+
+        playerSelectedFilePath = SpecialFolder.MyDocuments+"";
+        //DirectorySecurity sec = new DirectorySecurity();
+        //sec.AddAccessRule(new FileSystemAccessRule("S-1-5-32-545", FileSystemRights.FullControl, AccessControlType.Allow));
+        Debug.Log(playerSelectedFilePath);
+        playerSelectedFilePath = Path.GetFullPath(playerSelectedFilePath);
+        Debug.Log(playerSelectedFilePath);
+
+
+        Directory.CreateDirectory(playerSelectedFilePath + "/TheLostGame");
     }
 
     public void SendFileToPlayer(string fileName)
     {
+        /*
         if (!File.Exists(playerSelectedFilePath + "/" + fileName))
         {
             File.Delete(playerSelectedFilePath + "/" + fileName);
         }
+        */
+        File.Delete(playerSelectedFilePath + "/" + fileName);
         File.Create(playerSelectedFilePath + "/" + fileName).Dispose();
+        Debug.Log(sourceFilePath);
+        Debug.Log(playerSelectedFilePath);
         File.Copy(FolderSingleton.instance.sourceFilePath + "/" + fileName, playerSelectedFilePath + "/" + fileName, true);
     }
 }
